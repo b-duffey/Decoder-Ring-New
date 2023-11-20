e19688a774eb5f89780aa7a4c60f3a0cd5a406fb
Briana D <148458605+bd664-998@users.noreply.github.com>
2023-11-20 16:25:32 -0500

    Update caesar.js

--- scripts/caesar.js
+++ scripts/caesar.js
@@ -1,51 +1,25 @@
-const caesarModule = (function () {
-  function caesar(input, shift, encode = true) {
-    // Validate the input
-    if (
-      !input ||
-      typeof shift !== "number" ||
-      shift < -25 ||
-      shift > 25 ||
-      shift === 0
-    ) {
-      return false;
+function caesarRenderer() {
+  const form = document.querySelector("#caesar");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = event.target["caesar-input"].value;
+    const direction = event.target["caesar-options"].value;
+    const shift = event.target["caesar-shift"].value;
+    const result =
+      direction === "encode"
+        ? caesarModule.caesar(input, Number(shift))
+        : caesarModule.caesar(input, Number(shift), false);
+
+    const alert = document.querySelector("#caesar-alert");
+    if (result) {
+      alert.classList.add("d-none");
+      const output = document.querySelector("#caesar-output");
+      output.innerHTML = result;
+    } else {
+      alert.classList.remove("d-none");
     }
+  });
+}
 
-    // Perform the Caesar cipher
-    let result = "";
-    for (let i = 0; i < input.length; i++) {
-      const currentChar = input[i];
-
-      if (currentChar === " ") {
-        // Preserve spaces
-        result += " ";
-      } else if (/[a-z]/i.test(currentChar)) {
-        // Process alphabetic characters
-        const isUpperCase = currentChar === currentChar.toUpperCase();
-        const charCode = currentChar.toLowerCase().charCodeAt(0);
-
-        let newIndex;
-        if (encode) {
-          // Shift to the right (Caesar cipher)
-          newIndex = ((((charCode - 97 + shift) % 26) + 26) % 26) + 97;
-        } else {
-          // Shift to the left (decoding in the opposite direction)
-          newIndex = ((((charCode - 97 - shift + 26) % 26) + 26) % 26) + 97;
-        }
-
-        const newChar = String.fromCharCode(newIndex);
-        result += newChar.toLowerCase();
-      } else {
-        result += currentChar;
-      }
-    }
-
-    return result.toLowerCase();
-  }
-
-  return {
-    caesar,
-  };
-})();
-
-module.exports = { caesar: caesarModule.caesar };
+document.addEventListener("DOMContentLoaded", caesarRenderer);
